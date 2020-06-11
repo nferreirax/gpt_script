31cbf7157d845c98b9bc6a1beee89f0ca381507c
Nelson Ferreira <nferreirax@gmail.com>
2020-06-11 17:36:28 +0100

    InfyOm crud generator integration

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        \InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class, 
+
     ],
 
     /*
@@ -226,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
 
     ],
 
