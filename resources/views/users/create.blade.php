c484af273a3e14fb532f5ca999ac9f2fb0350fd9
Nelson Ferreira <nferreirax@gmail.com>
2020-06-16 14:24:13 +0100

    user crud change of crud generator base views

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,22 +1,34 @@
 @extends('adminlte::page')
-@section('title', 'User')
+@section('title', 'Create User')
 @section('content_header')
-    <h1>User</h1>
+    <h1>Create User</h1>
 @stop
 
 @section('content')
-    <div class="content">
-        @include('adminlte-templates::common.errors')
-        <div class="box box-primary">
-            <div class="box-body">
-                <div class="row">
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            @include('adminlte-templates::common.errors')
+            <!-- left column -->
+            <div class="col-md-12">
+                <!-- general form elements -->
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Create User</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
                     {!! Form::open(['route' => 'users.store']) !!}
 
                         @include('users.fields')
 
                     {!! Form::close() !!}
+
                 </div>
+                <!-- /.card -->
             </div>
         </div>
-    </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</section>
 @endsection
